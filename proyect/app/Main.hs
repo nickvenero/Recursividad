01d6512b140efa6f3d5b611642a34b05c8edf86e
nickvenero <nickvenero@gmail.com>
2023-06-06 16:40:52 -0300

    cambio 1

---
+++ proyect/app/Main.hs
@@ -0,0 +1,13 @@
+module Main (main) where
+import Lib
+main :: IO ()
+main = someFunc
+import Text.Show.Functions ()
+
+length' :: [a] -> Int
+length' []        = 0                   -- caso base
+length' (_:xs) = 1 + length' xs 
+
+
+elkfmelf
+
